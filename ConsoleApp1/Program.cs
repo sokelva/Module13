8af05e181564e5eedefa0efd3e5679fd15a1b53a
elsokolova <elsokolova@korusconsulting.ru>
2023-02-18 20:15:39 +0300

    добавлен проект

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace CountWords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Сохраняем предложение в строку
+            var sentence =
+                "Подсчитайте, сколько уникальных символов в этом предложении, используя HashSet<T>, учитывая знаки препинания, но не учитывая пробелы в начале и в конце предложения.";
+
+            // сохраняем в массив char
+            var characters = sentence.ToCharArray();
+
+            var symbols = new HashSet<char>();
+
+            // добавляем во множество. Сохраняются только неповторяющиеся символы
+            foreach (var symbol in characters)
+                symbols.Add(symbol);
+
+            // Выводим результат
+            Console.WriteLine(symbols.Count);
+        }
+    }
+}
